caa59034a6b939e2eda08bbb52dd4ed6d798c2bc
Chuyongwei <1429303705@qq.com>
2024-10-06 00:36:17 +0800

    prefect code about L2 relgular , drop node

--- deep/course2Week1-1.py
+++ deep/course2Week1-1.py
@@ -101,8 +101,9 @@ def model(X,Y,learning_rate=0.3,num_iterations=30000,print_cost=True,is_plot=Tru
     
     #返回学习后的参数
     return parameters
-
-# =========不用正则化
+# INFO 正则化
+#  =========  不用正则化
+'''
 parameters = model(train_X, train_Y,is_plot=True)
 print("训练集:")
 predictions_train = reg_utils.predict(train_X, train_Y, parameters)
@@ -114,6 +115,7 @@ axes = plt.gca()
 axes.set_xlim([-0.75,0.40])
 axes.set_ylim([-0.75,0.65])
 reg_utils.plot_decision_boundary(lambda x: reg_utils.predict_dec(parameters, x.T), train_X, train_Y)
+'''
 
 # ========L2正则化
 
@@ -183,6 +185,7 @@ def backward_propagation_with_regularization(X, Y, cache, lambd):
     
     return gradients
     
+'''
 parameters = model(train_X, train_Y, lambd=0.7,is_plot=True)
 print("使用正则化，训练集:")
 predictions_train = reg_utils.predict(train_X, train_Y, parameters)
@@ -195,6 +198,98 @@ axes = plt.gca()
 axes.set_xlim([-0.75,0.40])
 axes.set_ylim([-0.75,0.65])
 reg_utils.plot_decision_boundary(lambda x: reg_utils.predict_dec(parameters, x.T), train_X, train_Y)
+
+'''
+
+#===== 随机删除校验点
+
+def forward_propagation_with_dropout(X,parameters,keep_prob=0.5):
+    """
+    实现具有随机舍弃节点的前向传播。
+    LINEAR -> RELU + DROPOUT -> LINEAR -> RELU + DROPOUT -> LINEAR -> SIGMOID.
+    
+    参数：
+        X  - 输入数据集，维度为（2，示例数）
+        parameters - 包含参数“W1”，“b1”，“W2”，“b2”，“W3”，“b3”的python字典：
+            W1  - 权重矩阵，维度为（20,2）
+            b1  - 偏向量，维度为（20,1）
+            W2  - 权重矩阵，维度为（3,20）
+            b2  - 偏向量，维度为（3,1）
+            W3  - 权重矩阵，维度为（1,3）
+            b3  - 偏向量，维度为（1,1）
+        keep_prob  - 随机删除的概率，实数
+    返回：
+        A3  - 最后的激活值，维度为（1,1），正向传播的输出
+        cache - 存储了一些用于计算反向传播的数值的元组
+    """
+    np.random.seed(1)
+    
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    W3 = parameters["W3"]
+    b3 = parameters["b3"]
+    
+    #LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID
+    Z1 = np.dot(W1,X) + b1
+    A1 = reg_utils.relu(Z1)
+    
+    #下面的步骤1-4对应于上述的步骤1-4。
+    D1 = np.random.rand(A1.shape[0],A1.shape[1])    #步骤1：初始化矩阵D1 = np.random.rand(..., ...)
+    D1 = D1 < keep_prob                             #步骤2：将D1的值转换为0或1（使​​用keep_prob作为阈值）
+    A1 = A1 * D1                                    #步骤3：舍弃A1的一些节点（将它的值变为0或False）
+    A1 = A1 / keep_prob                             #步骤4：缩放未舍弃的节点(不为0)的值
+    """
+    #不理解的同学运行一下下面代码就知道了。
+    import numpy as np
+    np.random.seed(1)
+    A1 = np.random.randn(1,3)
+    
+    D1 = np.random.rand(A1.shape[0],A1.shape[1])
+    keep_prob=0.5
+    D1 = D1 < keep_prob
+    print(D1)
+    
+    A1 = 0.01
+    A1 = A1 * D1
+    A1 = A1 / keep_prob
+    print(A1)
+    """
+    
+    Z2 = np.dot(W2,A1) + b2
+    A2 = reg_utils.relu(Z2)
+    
+    #下面的步骤1-4对应于上述的步骤1-4。
+    D2 = np.random.rand(A2.shape[0],A2.shape[1])    #步骤1：初始化矩阵D2 = np.random.rand(..., ...)
+    D2 = D2 < keep_prob                             #步骤2：将D2的值转换为0或1（使​​用keep_prob作为阈值）
+    A2 = A2 * D2                                    #步骤3：舍弃A1的一些节点（将它的值变为0或False）
+    A2 = A2 / keep_prob                             #步骤4：缩放未舍弃的节点(不为0)的值
+    
+    Z3 = np.dot(W3, A2) + b3
+    A3 = reg_utils.sigmoid(Z3)
+    
+    cache = (Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3)
+    
+    return A3, cache
+
+parameters = model(train_X, train_Y, keep_prob=0.86, learning_rate=0.3,is_plot=True)
+
+print("使用随机删除节点，训练集:")
+predictions_train = reg_utils.predict(train_X, train_Y, parameters)
+print("使用随机删除节点，测试集:")
+reg_utils.predictions_test = reg_utils.predict(test_X, test_Y, parameters)
+
+plt.title("Model with dropout")
+axes = plt.gca()
+axes.set_xlim([-0.75, 0.40])
+axes.set_ylim([-0.75, 0.65])
+reg_utils.plot_decision_boundary(lambda x: reg_utils.predict_dec(parameters, x.T), train_X, train_Y)
+
+
+
+# INFO 梯度校验
+
 
 
 #%%
